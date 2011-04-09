8a783f5be3d49007ee3c606f4fd1cf11bac79144
Nick Saika <nicksaika@gmail.com>
2011-04-09 11:42:13 -0400

    New release (0.3)! Admittedly, this release is due to stagnation of the current codebase, but with good reason. A big change has been added which keeps XSP from bombing out when there is a comment node above the document element.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name = "xsp", 
-	version = "0.2", 
+	version = "0.3", 
 	py_modules = ["xsp"],
 	author = "Nick Saika",
 	author_email = "nicksaika@gmail.com")
